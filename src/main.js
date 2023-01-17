232fe27dd24291ce0374854d57e452b6707a213f
Cristian David Corrales Ospina <ccorrales@PASCUALBRAVO.CENTRAL>
2023-01-17 13:05:18 -0500

    Se finalizó maquetado y se realizó modal - pendiente agregar imágenes y subir proyecto al repositorio completo y a platzi

---
+++ src/main.js
@@ -0,0 +1,13 @@
+const btn1=document.getElementById("btn1");
+const modal=document.querySelector(".modal");
+const btnClose=document.querySelector(".modal-content-close");
+
+btn1.addEventListener("click",()=>{
+    modal.classList.remove("hidden");
+    
+})
+
+btnClose.addEventListener("click",()=>{
+   
+    modal.classList.add("hidden");
+})
